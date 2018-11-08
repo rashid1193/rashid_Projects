00e4fb46d736fb34286a47b1f05607e6ec40276e
ZMQ MAC4 <zmqmac4@zmqs-mac-mini.local>
2018-11-08 15:11:13 +0530

    First Commmmitttt....

--- GitProjectNumber1/ViewController.swift
+++ GitProjectNumber1/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Print First Item.")
+        print("Print Second Items.......")
     }
 
     override func didReceiveMemoryWarning() {
