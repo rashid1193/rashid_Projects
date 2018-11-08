b3eb0fb861595dda5b0893b94dbd1e152af325d6
ZMQ MAC4 <zmqmac4@zmqs-mac-mini.local>
2018-11-08 11:58:42 +0530

    Initial Commit

---
+++ GitProjectNumber1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitProjectNumber1
+//
+//  Created by ZMQ MAC4 on 08/11/18.
+//  Copyright © 2018 ZMQ MAC4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
