6f2b60b6ac489e9eb4ca3dd7108dea354c8cdae3
ZMQ MAC4 <zmqmac4@zmqs-mac-mini.local>
2018-11-12 18:03:55 +0530

    commit adddddinggggg......

--- GitProjectNumber1/ViewController.swift
+++ GitProjectNumber1/ViewController.swift
@@ -8,21 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad()
-    {
+class ViewController: UIViewController
+{
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Print First Item.")
-        print("Print Second Items.......")
+        print("Print First  Item.")
+        print("Print Second Item.")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
